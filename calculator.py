ca039de2e37d832c30522579053006a3c65443ac
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-01-02 21:19:37 -0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,51 @@
+from tkinter import Tk, Entry, Button, StringVar
+
+
+class Calculator:
+    def __ini__(self, master):
+        master.title("Calculator")
+        master.geometry('357x420+0+0')
+        master.config(bg='gray')
+        master.resizable(False, False)
+
+        self.equation=StringVar()
+        self.entry_value=''
+        Entry(width=17, bg='#fff', font=('Arial Bold',28), textvariable=self.equation).place(x=0,y=0)
+        
+        Button(width=11,height=4, text='(',relief='flat',bg='white',command=lambda:self.show('(').placec(x= ,y=)
+        Button(width=11,height=4, text=')',relief='flat',bg='white',command=lambda:self.show(')').placec(x= ,y=)
+        Button(width=11,height=4, text='%',relief='flat',bg='white',command=lambda:self.show('%').placec(x= ,y=)
+        Button(width=11,height=4, text='1',relief='flat',bg='white',command=lambda:self.show(1).placec(x= ,y=)
+        Button(width=11,height=4, text='2',relief='flat',bg='white',command=lambda:self.show(2).placec(x= ,y=)
+        Button(width=11,height=4, text='3',relief='flat',bg='white',command=lambda:self.show(3).placec(x= ,y=)
+        Button(width=11,height=4, text='4',relief='flat',bg='white',command=lambda:self.show(4).placec(x= ,y=)
+        Button(width=11,height=4, text='5',relief='flat',bg='white',command=lambda:self.show(5).placec(x= ,y=)
+        Button(width=11,height=4, text='6',relief='flat',bg='white',command=lambda:self.show(6).placec(x= ,y=)
+        Button(width=11,height=4, text='7',relief='flat',bg='white',command=lambda:self.show(7).placec(x= ,y=)
+        Button(width=11,height=4, text='8',relief='flat',bg='white',command=lambda:self.show(8).placec(x= ,y=)
+        Button(width=11,height=4, text='9',relief='flat',bg='white',command=lambda:self.show(9).placec(x= ,y=)
+        Button(width=11,height=4, text='0',relief='flat',bg='white',command=lambda:self.show(0).placec(x= ,y=)
+        Button(width=11,height=4, text='.',relief='flat',bg='white',command=lambda:self.show('.').placec(x= ,y=)
+        Button(width=11,height=4, text='+',relief='flat',bg='white',command=lambda:self.show('+').placec(x= ,y=)
+        Button(width=11,height=4, text='-',relief='flat',bg='white',command=lambda:self.show('-').placec(x= ,y=)
+        Button(width=11,height=4, text='/',relief='flat',bg='white',command=lambda:self.show('/').placec(x= ,y=)
+        Button(width=11,height=4, text='x',relief='flat',bg='white',command=lambda:self.show('x').placec(x= ,y=)
+        Button(width=11,height=4, text='=',relief='flat',bg='white',command=lambda:self.show('=').placec(x= ,y=)
+        Button(width=11,height=4, text='C',relief='flat',bg='white',command=lambda:self.show('C').placec(x= ,y=)
+   
+   
+    def show(self, value):
+        self.entry_value+=str(value)
+        self.equation.set(self.entry_value)
+
+    def clear(self):
+        self.entry_value=''
+        self.equation.set(self.entry_value)
+
+    def solve(self):
+        result=eval(self.entry_value)
+        self.equation.set(result)
+
+root = Tk()
+Calculator=Calculator(root)
+root.mainloop()
