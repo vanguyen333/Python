aa90318c287050d45301643935715c3f9865032d
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-01-02 21:39:22 -0500

    add some numbers to x and y

--- calculator.py
+++ calculator.py
@@ -12,12 +12,12 @@ class Calculator:
         self.entry_value=''
         Entry(width=17, bg='#fff', font=('Arial Bold',28), textvariable=self.equation).place(x=0,y=0)
         
-        Button(width=11,height=4, text='(',relief='flat',bg='white',command=lambda:self.show('(').placec(x= ,y=)
-        Button(width=11,height=4, text=')',relief='flat',bg='white',command=lambda:self.show(')').placec(x= ,y=)
-        Button(width=11,height=4, text='%',relief='flat',bg='white',command=lambda:self.show('%').placec(x= ,y=)
-        Button(width=11,height=4, text='1',relief='flat',bg='white',command=lambda:self.show(1).placec(x= ,y=)
-        Button(width=11,height=4, text='2',relief='flat',bg='white',command=lambda:self.show(2).placec(x= ,y=)
-        Button(width=11,height=4, text='3',relief='flat',bg='white',command=lambda:self.show(3).placec(x= ,y=)
+        Button(width=11,height=4, text='(',relief='flat',bg='white',command=lambda:self.show('(').placec(x=0,y=50)
+        Button(width=11,height=4, text=')',relief='flat',bg='white',command=lambda:self.show(')').placec(x=90 ,y=50)
+        Button(width=11,height=4, text='%',relief='flat',bg='white',command=lambda:self.show('%').placec(x=180 ,y=50)
+        Button(width=11,height=4, text='1',relief='flat',bg='white',command=lambda:self.show(1).placec(x= 0,y=125)
+        Button(width=11,height=4, text='2',relief='flat',bg='white',command=lambda:self.show(2).placec(x= 90,y=125)
+        Button(width=11,height=4, text='3',relief='flat',bg='white',command=lambda:self.show(3).placec(x=180 ,y=125)
         Button(width=11,height=4, text='4',relief='flat',bg='white',command=lambda:self.show(4).placec(x= ,y=)
         Button(width=11,height=4, text='5',relief='flat',bg='white',command=lambda:self.show(5).placec(x= ,y=)
         Button(width=11,height=4, text='6',relief='flat',bg='white',command=lambda:self.show(6).placec(x= ,y=)
